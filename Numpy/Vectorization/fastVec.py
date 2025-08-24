29d37eb6f39d8ae89de4e92d6f5d42821501597d
Pritam Roy <lightyagami180404@gmail.com>
2025-08-24 18:22:42 +0530

    Vectorization

---
+++ Numpy/Vectorization/fastVec.py
@@ -0,0 +1,10 @@
+#vectorized addition
+
+import numpy as np
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([10,20,30])
+
+result = arr1 + arr2
+
+print(result)
---
+++ Numpy/Vectorization/fastVec2.py
@@ -0,0 +1,12 @@
+#Vectorized multi.
+
+import numpy as np
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+result = arr1 * arr2
+print(result)
+
+multiplied = result * 3
+print(multiplied)
